c9f7cf5107172b2075364fd0a758c468d473ba5e
Sopedia <soran.hassan21@gmail.com>
2024-12-09 14:46:00 +0330

    Update db.py

--- database/db.py
+++ database/db.py
@@ -2,6 +2,4 @@
     Datebase of Test
 """
 
-pattern = {
-    "پرستاری": "https://iranausbildung.com/%d8%a2%d9%88%d8%b3%d8%a8%db%8c%d9%84%d8%af%d9%88%d9%86%da%af-%d9%be%d8%b1%d8%b3%d8%aa%d8%a7%d8%b1%db%8c/"
-}
+pattern = {}
